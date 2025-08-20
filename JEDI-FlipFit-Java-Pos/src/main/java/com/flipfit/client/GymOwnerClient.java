b7fdde6a19c58e641a4d47b354a53132e49c43e0
Soham-Jadhav <soham.jadhav.21031@iitgoa.ac.in>
2025-08-20 10:07:56 +0530

    Reverting to c01989a

--- JEDI-FlipFit-Java-Pos/src/main/java/com/flipfit/client/GymOwnerClient.java
+++ JEDI-FlipFit-Java-Pos/src/main/java/com/flipfit/client/GymOwnerClient.java
@@ -1,6 +1,8 @@
 package com.flipfit.client;
 
 import com.flipfit.bean.GymOwner;
+import com.flipfit.business.GymOwnerBusiness;
+import com.flipfit.business.UserBusiness;
 
 import java.util.Scanner;
 
