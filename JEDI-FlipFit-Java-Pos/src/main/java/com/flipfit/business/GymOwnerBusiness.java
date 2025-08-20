39c1a1dee0021f2a0cc790189747ae05e3f58001
Soham-Jadhav <soham.jadhav.21031@iitgoa.ac.in>
2025-08-20 22:54:12 +0530

    Add gym owner business

--- JEDI-FlipFit-Java-Pos/src/main/java/com/flipfit/business/GymOwnerBusiness.java
+++ JEDI-FlipFit-Java-Pos/src/main/java/com/flipfit/business/GymOwnerBusiness.java
@@ -1,40 +1,43 @@
 package com.flipfit.business;
 import java.util.*;
-//import com.flipfit.dao.GymOwnerDAOImpl;
+import com.flipfit.dao.GymOwnerDAOImpl;
 import com.flipfit.bean.GymOwner;
-//import com.flipfit.dao.UserDAOImpl;
+import com.flipfit.dao.UserDAOImpl;
 import com.flipfit.bean.GymCenter;
-//import com.flipfit.dao.GymOwnerDAOImpl;
 import com.flipfit.bean.Slot;
 
 public class GymOwnerBusiness implements GymOwnerBusinessInterface {
-//    UserDAOImpl userDAO = new UserDAOImpl();
-//    GymOwnerDAOImpl gymOwnerDAO = new GymOwnerDAOImpl();
+    UserDAOImpl userDAO = new UserDAOImpl();
+    GymOwnerDAOImpl gymOwnerDAO = new GymOwnerDAOImpl();
 
     public GymOwner getProfile(String email) {
-        // Implementation removed
-        return null;
+        System.out.println("Fetched Gym owner details successfully! " + email);
+        return userDAO.getGymOwnerDetails(email);
     }
 
     public void editProfile(GymOwner gymOwnerNew) {
-        // Implementation removed
+        userDAO.editGymOwnerDetails(gymOwnerNew);
+        System.out.println("\nEdited your profile Successfully!");
     }
 
     public boolean addGym(GymCenter gym) {
-        // Implementation removed
-        return false;
+        gymOwnerDAO.addGym(gym);
+        System.out.println("\nAdded Gym Successfully!" + gym.getGymId());
+        return true;
     }
 
     public void editGym(GymCenter gym) {
-        // Implementation removed
+        gymOwnerDAO.editGym(gym);
+        System.out.println("\nEdited Gym Details Successfully! ");
     }
 
     public void addSlot(Slot slot) {
-        // Implementation removed
+        gymOwnerDAO.addSlot(slot);
+        System.out.println("\nAdded slot successfully!");
     }
 
     public List<GymCenter> getGymDetail(String gymOwnerEmail) {
-        // Implementation removed
-        return null;
+        System.out.println("\nFetched gym details successfully! " + gymOwnerEmail);
+        return gymOwnerDAO.getGymsOfGymOwner(gymOwnerEmail);
     }
 }
